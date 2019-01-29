48a85413f41026257858daf0ca1488870623038b
Alexis Saettler <alexis@saettler.org>
2019-01-29 09:32:20 +0100

    add circleci

--- src/Commands/UpdateTranslations.php
+++ src/Commands/UpdateTranslations.php
@@ -26,7 +26,7 @@ class UpdateTranslations extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      */
     public function handle()
     {
